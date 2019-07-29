307e347de3a9004ef9c42155412380985896a9bf
Yasser Alexander Perez Urariyu (Analista de Aplicaciones Senior) <yaaperez@colsanitas.com>
2019-07-29 15:49:41 -0500

    Ajutes nombre de archivo navigator Main

---
+++ src/navigators/NavigatorMain.js
@@ -0,0 +1,39 @@
+import LoginScreen from '../screens/LoginScreen'
+import CreateUserScreen from '../screens/user/CreateUserScreen'
+import AuthLoadingScreen from '../screens/AuthLoadingScreen'
+import TabHomeNavigator from './TabHomeNavigator'
+import HomeDrawerNavigator from './HomeDrawerNavigator'
+
+import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
+
+export const LoginStack = createStackNavigator({
+    LoginIn: LoginScreen,
+    CreateUser: CreateUserScreen
+}, {
+        defaultNavigationOptions: ({ navigation }) => {
+            return {
+                title: 'Brigada de Salud',
+                headerStyle: {
+                    backgroundColor: 'red'
+                },
+                headerTitleStyle: {
+                    color: 'white'
+                }
+            }
+        }
+    }, {
+        initialRouteName: 'LoginIn'
+    });
+
+export const NavigatorMain = createSwitchNavigator(
+    {
+        AuthLoading: AuthLoadingScreen,
+        Auth: LoginStack,
+        Home: TabHomeNavigator,
+        HomeDrawer: HomeDrawerNavigator
+
+    },
+    {
+        initialRouteName: 'AuthLoading',
+    }
+);
